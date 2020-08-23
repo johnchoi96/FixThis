b1367a455af00f2305074900b51576038595c71c
John Choi <johnchoi1003@icloud.com>
2020-08-23 18:15:59 -0400

    Initial commit

---
+++ FixThis/FixThisManager.swift
@@ -0,0 +1,13 @@
+//
+//  FixThisManager.swift
+//  FixThis
+//
+//  Created by John Choi on 8/23/20.
+//  Copyright © 2020 John Choi. All rights reserved.
+//
+
+import Foundation
+
+struct FixThisManager {
+    
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# FixThis-iOS
